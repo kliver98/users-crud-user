a45e4609b83fd24cb68d53c89d5bfda43562cb98
Kliver Daniel <36433884+kliver98@users.noreply.github.com>
2021-05-24 02:54:10 -0500

    Kliver fixes (#1)
    * add verification to check if given id for delete its a positive number and return better status code
    
    * added better status code to return and verifications

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,12 @@
 const User = require('../models/users')
 
+var positive_numbers = new RegExp('^\\d+$')
+
 exports.index = function (req, res, next) {
     User.find({}, (err, users) => {
         if (err)
             return next(err)
+        users = users.map(user => {return {username:user.username,_id:user._id}})
         res.send(users);
     })
 }
@@ -20,7 +23,9 @@ exports.create = function(req, res, next) {
         active: req.body.active,
     });
 
-    user.save(err => {
+    user.save((err,user_saved) => {
+        if (!user_saved)
+            return res.status(400).send(`Error: id ${id} already exist`)
         if (err)
             return next(err)
         res.send("User ["+user._id+"] created successfully")
@@ -32,22 +37,39 @@ exports.show = function (req, res, next) {
     User.findById(id, (err, user) => {
         if (err)
             return next(err)
+        user.password=''
         res.send(user)
     })
 }
 
 exports.delete = function (req, res, next) {
-    User.findByIdAndRemove(req.params.id, (err, user) => {
+    let id = req.params.id
+    if (!positive_numbers.test(id))
+        return res.status(400).send(`${id} is not valid id`)
+    User.findByIdAndRemove(id, (err, user) => {
         if (err)
             return next(err)
-        res.send("User ["+user._id+"] deleted successfully")
+        else {
+            if (user)
+                res.status(200).send("User ["+user._id+"] deleted successfully")
+            else
+                res.status(400).send(`Error: id ${id} does not exist`)
+        }
     })
 }
 
 exports.update = function (req, res, next) {
-    User.findByIdAndUpdate(req.params.id, {$set: req.body}, (err, user) => {
+    let id = req.params.id
+    if (!positive_numbers.test(id))
+        return res.status(400).send(`${id} is not valid id`)
+    User.findByIdAndUpdate(id, {$set: req.body}, (err, user) => {
         if (err)
             return next(err)
-        res.send("User ["+user._id+"] updated successfully")
+        if (!user)
+            res.status(400).send(`Error: id ${id} does not exist`)
+        else if (user._id!==parseInt(id))
+            res.status(400).send(`Error: ids does not match, cannot update id`)
+        else
+            res.status(200).send("User ["+user._id+"] updated successfully")
     })
 }
