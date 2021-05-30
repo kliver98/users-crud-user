f54d0e9c2b19e3d032d9f2bb0f54914a7f8026a4
Kliver Daniel <kliver1998@gmail.com>
2021-05-30 12:08:34 -0500

    finish contract test with basic list, create and delete

--- test/contract/api/user-service.spec.js
+++ test/contract/api/user-service.spec.js
@@ -6,16 +6,27 @@ const app = require('express')();
 app.use('/api/latest/users', require('../../../routes/users'));
 const server = app.listen("3000");
 
-let users = [{
-    _id:100000,
+//Variables needed
+const user_id = 100001
+const user_id_created = 100002
+const users = [{
+    _id:0,
     username:"admin"
 }]
+const message_deleted = `Information: User [${user_id}] deleted successfully`
+const message_created = `Information: user with id [${user_id_created}] created successfully`
 
 describe("Pact Verification", () => {
-    beforeEach(() => {
+    beforeAll(() => {
+        nock(process.env.API)
+          .delete(`/api/latest/users/${user_id}`)
+          .reply(200, message_deleted);
         nock(process.env.API)
           .get('/api/latest/users')
           .reply(200, users);
+        nock(process.env.API)
+          .post('/api/latest/users')
+          .reply(201, message_created);
     });
 
     it("validates the expectations of UserService", () => {
@@ -27,16 +38,14 @@ describe("Pact Verification", () => {
             pactBrokerToken: process.env.PACT_BROKER_TOKEN
         };
 
-        if (process.env.CI || process.env.PACT_PUBLISH_RESULTS) {
-            Object.assign(opts, {
-                publishVerificationResult: true,
-            });
-        }
-
         return new Verifier(opts).verifyProvider().then(output => {
             // console.log(output);
         }).finally(() => {
             server.close();
         });
     })
+
+    afterAll(() => {
+        server.close();
+    });
 });
